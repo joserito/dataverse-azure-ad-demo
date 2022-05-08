7903a421713e6a333e67cc70f85ecbd4b1071e3d
joserito <rito.martinez@hotmail.com>
2022-05-08 10:04:55 -0500

    Adding server extensions project

---
+++ DataverseAzureAd/Dataverse.ServerExtensions/CustomApi/CreateAccount.cs
@@ -0,0 +1,14 @@
+﻿using Dataverse.ServerExtensions.Framework;
+using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Dataverse.ServerExtensions.CustomApi
+{
+    public class CreateAccount : PluginBase
+    {
+        protected override void ExecuteLogic()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataverseAzureAd/Dataverse.ServerExtensions/Framework/PluginBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Dataverse.ServerExtensions.Framework
+{
+    public abstract class PluginBase : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected abstract void ExecuteLogic();
+    }
+}
